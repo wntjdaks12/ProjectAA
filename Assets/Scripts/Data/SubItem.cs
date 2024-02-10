8604c199e6697d3a603a8282b652886aefb3a212
DESKTOP-NBIU3MK\workstation <qqqq8692@naver.com>
2024-02-10 20:55:14 +0900

    스탯 추가

--- Assets/Scripts/Data/SubItem.cs
+++ Assets/Scripts/Data/SubItem.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SubItem : Data
+public class SubItem : Data, IStat
 {
+    public StatAbility StatAbility { get; set ; }
+
+    public void Init()
+    {
+        var stat = GameApplication.Instance.GameModel.PresetData.ReturnData<Stat>(nameof(Stat), Id);
+        StatAbility = new StatAbility(stat);
+    }
 }
